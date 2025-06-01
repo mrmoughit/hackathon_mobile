6a4be3f383dd2206e3ef926bc69322cc8d4404b5
ABDOLMOUGHIT ECHCHARBINY <111705320+mrmoughit@users.noreply.github.com>
2025-06-01 16:40:54 +0100

    abdelmoughit

--- backend/app.js
+++ backend/app.js
@@ -232,7 +232,7 @@ app.post('/addevent', upload.single('image'), async (req, res) => {
     return res.status(400).json("Missing required data");
   }
 
-  const image = req.file ? `/uploads/${req.file.filename}` : null;
+  const image = req.file ? `http://13.60.16.112/uploads/${req.file.filename}` : null;
   const time24h = convert_houre(time);
   const eventDateTime = new Date(`${date}T${time24h}`);
 
