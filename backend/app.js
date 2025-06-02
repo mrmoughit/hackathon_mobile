1d21012327907401489eacc96cce5c22a79bddf3
ABDOLMOUGHIT ECHCHARBINY <111705320+mrmoughit@users.noreply.github.com>
2025-06-02 06:39:09 +0100

    abdelmoughit

--- backend/app.js
+++ backend/app.js
@@ -615,7 +615,6 @@ app.post('/add/saved/event', async (req, res) => {
       return res.status(401).json({ error: "Token missing or invalid" });
   }
   
-  console.log(req.body);
   const event_id = req.body.event_id;
   if (!event_id) return res.status(400).json({ error: "Missing event ID" });
 
@@ -638,6 +637,144 @@ app.post('/add/saved/event', async (req, res) => {
   }
 });
 
+
+
+app.post('/add_registration', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+      return res.status(401).json({ error: "Token missing or invalid" });
+  }
+
+  const event_id = req.body.event_id;
+  if (!event_id) {
+      return res.status(400).json({ error: "Missing event ID" });
+  }
+
+  try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const userLogin = decoded.login;
+      
+      const id = await get_user_id(userLogin);
+      if (id === -1) {
+          return res.status(404).json({ error: "User not found" });
+      }
+
+      await pool.query('INSERT INTO registration (user_id, event_id) VALUES (?, ?)', [id, event_id]);
+
+      res.status(201).json({ message: "Registered successfully" });
+
+  } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+  }
+})
+
+
+app.get('/get_registration', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+      return res.status(401).json({ error: "Token missing or invalid" });
+  }
+
+  try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const userLogin = decoded.login;
+
+      const id = await get_user_id(userLogin);
+      if (id === -1) {
+          return res.status(404).json({ error: "User not found" });
+      }
+
+      const [result] = await pool.query('SELECT * FROM registration WHERE user_id = ?', [id]);
+
+      res.status(200).json(result);
+
+  } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+
+
+
+app.get('/is_resgiter', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+      return res.status(401).json({ error: "Token missing or invalid" });
+  }
+
+  const event_id = req.query.event_id; 
+  if (!event_id) {
+      return res.status(400).json({ error: "Missing event ID" });
+  }
+
+  try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const userLogin = decoded.login;
+
+      const id = await get_user_id(userLogin);
+      if (id === -1) {
+          return res.status(404).json({ error: "User not found" });
+      }
+
+      const [result] = await pool.query(
+          'SELECT * FROM registration WHERE user_id = ? AND event_id = ?',
+          [id, event_id]
+      );
+
+      return res.status(200).json({ registered: result.length > 0 });
+  } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+
+app.delete('/delete_register', async (req, res) => {
+
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+      return res.status(401).json({ error: "Token missing or invalid" });
+  }
+
+  const event_id = req.query.event_id;
+  if (!event_id) {
+      return res.status(400).json({ error: "Missing event ID" });
+  }
+
+  try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const userLogin = decoded.login;
+
+      const id = await get_user_id(userLogin);
+      if (id === -1) {
+          return res.status(404).json({ error: "User not found" });
+      }
+
+      const [result] = await pool.query(
+          'DELETE FROM registration WHERE user_id = ? AND event_id = ?',
+          [id, event_id]
+      );
+
+      if (result.affectedRows === 0) {
+          return res.status(404).json({ error: "No registration found to delete" });
+      }
+
+      return res.status(200).json({ message: "Registration deleted successfully" });
+  } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal server error" });
+  }
+})
 
 const PORT = process.env.PORT || 3000;
 
